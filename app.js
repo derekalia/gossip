51a6330142172f02380d91e92da9bc4dbe3d22a0
Derek Alia <derekalia@me.com>
2018-04-08 13:49:06 -0700

    its alive

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+
+
+
+app.use('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(port, () => {
+  console.log('Server listening on port ' + port);
+});
